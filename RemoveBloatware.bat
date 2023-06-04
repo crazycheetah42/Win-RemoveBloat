38bd1d1840f9df8e5aaf5cbcc28a7598ab635c83
Aryaman Sriram <76403763+crazycheetah42@users.noreply.github.com>
2023-06-04 18:43:30 -0400

    Add files via upload

---
+++ RemoveBloatware.bat
@@ -0,0 +1,15 @@
+@ECHO off
+powershell Remove-AppxPackage Microsoft.3DBuilder
+powershell Remove-AppxPackage Microsoft.BingWeather
+powershell Remove-AppxPackage Microsoft.Calendar
+powershell Remove-AppxPackage Microsoft.Cortana
+powershell Remove-AppxPackage Microsoft.GetHelp
+powershell Remove-AppxPackage Microsoft.HEIFImageExtension
+powershell Remove-AppxPackage Microsoft.Mail
+powershell Remove-AppxPackage Microsoft.OneNote
+powershell Remove-AppxPackage Microsoft.People
+powershell Remove-AppxPackage Microsoft.SkypeApp
+powershell Remove-AppxPackage Microsoft.SolitaireCollection
+powershell Remove-AppxPackage Microsoft.StickyNotes
+powershell Remove-AppxPackage Microsoft.XboxGameBar
+powershell Remove-AppxPackage Microsoft.XboxGamingOverlay
